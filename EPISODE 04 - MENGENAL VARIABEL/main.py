89e596d6d17572d21e5c85135f27ced79703af88
devcode-gihh <mochamadsugianasaepul@gmail.com>
2025-09-24 18:51:02 +0700

    belajar python - mengenal variabel

---
+++ EPISODE 04 - MENGENAL VARIABEL/main.py
@@ -0,0 +1,26 @@
+# Variabel adalah tempat menyimpan data
+
+# menyimpan / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a =", a)
+print("Nilai x =", x)
+print("Nilai Panjang =", panjang)
+
+# penamaan variabel
+penamaan = 15 # penamaan normal
+penamaan2 = 20 # penamaan huruf & angka
+penamaan_3 = 30 # penamaan menggunakan underscore
+
+# pemanggilan kedua
+print("Nilai a =", a)
+a = 100 # apakah bisa merubah nilai variabel yang sudah ada? bisa
+print("Nilai a =", a,", ini berubah yang awalnya 10 menjadi 100")
+
+# assignment indirect
+b = a
+print("Nilai b =", b)
+
