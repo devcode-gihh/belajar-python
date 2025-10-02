9f4588f70de98d626968082821168990a149ade1
devcode-gihh <mochamadsugianasaepul@gmail.com>
2025-10-02 07:34:19 +0700

    belajar python - operator assignment

---
+++ EPISODE 14 - OPERATOR ASSIGNMENT/main.py
@@ -0,0 +1,37 @@
+# Operasi yang dapat dilakukan dengan penyingkatan
+# Operasi ditambah dengan assignment
+
+a = 5 # ini adalah assignment
+print("Nilai a =", a)
+
+a += 1 # artinya adalah a = a + 1
+print("Nilai a += 1, Nilai a menjadi", a)
+
+a -= 2 # artinya adalah a = a - 2
+print("Nilai a -= 2, Nilai a menjadi", a)
+
+a *= 5 # artinya adalah a = a * 5
+print("Nilai a *= 5, Nilai a menjadi", a)
+
+a /= 2 # artinya adalah a = a / 2
+print("Nilai a /= 2, Nilai a menjadi", a)
+
+print("-------------")
+b = 10
+print("Nilai b =", b)
+
+b %= 3 # artinya adalah b = b % 3
+print("Nilai b %= 3, Nilai b menjadi", b)
+
+b = 10
+print("Nilai b =", b)
+
+b //= 3 # artinya adalah b = b // 3
+print("Nilai b //= 3, Nilai b menjadi", b)
+
+print("-------------")
+a = 5 
+print("Nilai a =", a)
+
+a **= 3 # artinya adalah a = a ** 3
+print("Nilai a **= 3, Nilai a menjadi", a)
